88c4f3541d562e6d6929615856a8ade395c789dd
巴思 <basi.dwz@taobao.com>
2014-11-06 23:03:46 +0800

    添加上海&长沙

--- app.js
+++ app.js
@@ -21,8 +21,17 @@ var hbs = exphbs.create({
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
+var cities = [
+  {key: 'hangzhou', name: '浙江杭州'},
+  {key: 'shanghai', name: '上海'},
+  {key: 'beijing', name: '北京'},
+  {key: 'chengdu', name: '四川成都'},
+  {key: 'nanning', name: '广西南宁'},
+  {key: 'changsha', name: '湖南长沙'}
+];
+
 app.get('/', function (req, res, next) {
-  res.render('home');
+  res.render('home', {cities: cities});
 });
 
 // 针对各个地域的 route 配置
@@ -36,43 +45,20 @@ app.get('/all', function (req, res, next) {
   });
 });
 
-app.get('/hangzhou', function (req, res, next) {
-  Post.find({author_location: '浙江杭州'}).sort({create_at: -1}).limit(100).exec(function (err, docs) {
-    if (err) {
-      return next(err);
-    }
-    res.render('posts', {docs: docs});
-  });
-});
+for (var i = 0; i < cities.length; i++) {
+  (function (city) {
+    app.get('/city/' + city.key, function (req, res, next) {
+      Post.find({author_location: city.name}).sort({create_at: -1}).limit(100).exec(function (err, docs) {
+        if (err) {
+          return next(err);
+        }
+        res.render('posts', {docs: docs});
+      });
+    });
+  })(cities[i]);
+}
 
-app.get('/chengdu', function (req, res, next) {
-  Post.find({author_location: '四川成都'}).sort({create_at: -1}).limit(100).exec(function (err, docs) {
-    if (err) {
-      return next(err);
-    }
-    res.render('posts', {docs: docs});
-  });
-});
-
-app.get('/beijing', function (req, res, next) {
-  Post.find({author_location: '北京'}).sort({create_at: -1}).limit(100).exec(function (err, docs) {
-    if (err) {
-      return next(err);
-    }
-    res.render('posts', {docs: docs});
-  });
-});
-
-app.get('/nanning', function (req, res, next) {
-  Post.find({author_location: '广西南宁'}).sort({create_at: -1}).limit(100).exec(function (err, docs) {
-    if (err) {
-      return next(err);
-    }
-    res.render('posts', {docs: docs});
-  });
-});
 // END 针对各个地域的 route 配置
-
 
 
 // 启动爬虫
